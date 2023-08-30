06c6642dc8c1d225ecb8542f576b712c8b692008
emanuelebroggini <emanuele.broggini@mail.polimi.it>
2023-08-30 12:56:48 +0200

    Create include parameters file

--- FFT2D/src/main_test.cpp
+++ FFT2D/src/main_test.cpp
@@ -1,52 +1,33 @@
-#ifndef ROW_LENGTH
-#define ROW_LENGTH 4
-#endif
-#ifndef CHECK_CORRECTNESS
-#define CHECK_CORRECTNESS true
-#endif
-#ifndef TIME_IMPL
-#define TIME_IMPL true
-#endif
-#if TIME_IMPL
-#include <chrono>
-#ifndef NUM_ITER_TO_TIME
-#define NUM_ITER_TO_TIME 2
-#endif
-#else
-#define NUM_ITER_TO_TIME 1
-#endif
-#ifndef MAX_MAT_VALUES
-#define MAX_MAT_VALUES 250
-#endif
-
+#include "../inc/parameters"
 
 #include <iostream>
 #include <string.h>
-#include <iomanip>
-#include <complex>
-#include <cmath>
-#include <cstdlib>
+// #include <iomanip>
+// #include <cmath>
+// #include <cstdlib>
+
 #include <vector>
-#include <omp.h> 
 #include <complex>
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
-#include <unsupported/Eigen/SparseExtra> 
 
-#include "GetPot"
+#if TIME_IMPL
+#include <chrono>
+#endif
+
+#if PAR_IMPL
+#include <omp.h> 
+#endif
+
+#if SEQ_IMPL
+#endif
+
 #include "../inc/FFT_2D.hpp"
+#include "../../lib/GetPot"
 
-#ifndef SEQ_IMPL
-#define SEQ_IMPL true
-#endif
-
-#ifndef PAR_IMPL
-#define PAR_IMPL true
-#endif
 
 using namespace std;
 using SparseMat = Eigen::SparseMatrix<std::complex<double>>;
 
+#if CHECK_CORRECTNESS
 void DFT_2D(Mat& spatialValues, const unsigned int n) {
     Mat frequencyValues(n, n);
 
@@ -73,29 +54,7 @@ void DFT_2D(Mat& spatialValues, const unsigned int n) {
     }
 }
 
-template <class T>
-void DFT_2D(const std::vector<std::vector<T>> &spatialValues, std::vector<std::vector<std::complex<double>>> &frequencyValues) {
-    const unsigned int n = spatialValues.size();
-    frequencyValues.resize(n);
 
-    for (unsigned int k = 0; k < n; k++) {
-        frequencyValues[k].resize(n);
-        for (unsigned int l = 0; l < n; l++) {
-            std::complex<double> sum(0, 0);
-            for (unsigned int j = 0; j < n; j++) {
-                for (unsigned int i = 0; i < n; i++) {
-                    std::complex<double> term = static_cast<std::complex<double>>(spatialValues[i][j]) *
-                        std::exp(-2.0 * M_PI * std::complex<double>(0, 1) * static_cast<double>((k * i + l * j)) / static_cast<double>(n));
-                    
-                    sum += term;
-                }
-            }
-            frequencyValues[k][l] = sum;
-        }
-    }
-}
-
-#if CHECK_CORRECTNESS
 int checkCorrectness(const string implemName, const Mat &correct, const Mat &toCheck) {
     bool isCorrect = true;
     constexpr double eps(1e-10 * MAX_MAT_VALUES);
@@ -122,35 +81,6 @@ int checkCorrectness(const string implemName, const Mat &correct, const Mat &toC
     std::cout << "Correct transformation in " << implemName << "!" << endl;
     return 0;
 }
-
-template <class T>
-int checkCorrectness(const string implemName, const Mat &correct, const std::vector<std::vector<T>> &toCheck) {
-    bool isCorrect = true;
-    constexpr double eps(1e-10 * MAX_MAT_VALUES);
-
-    for (int i = 0; i < correct.rows(); ++i) {
-        for (int j = 0; j < correct.cols(); ++j) {
-            const std::complex<double> &correctValue = correct.coeff(i, j);
-            const std::complex<double> &toCheckValue = toCheck[i][j];
-
-            if (std::abs(correctValue.imag() - toCheckValue.imag()) > eps ||
-                std::abs(correctValue.real() - toCheckValue.real()) > eps) {
-                std::cout << "Problem with element at (" << i << ", " << j << "): " << toCheckValue
-                          << ", It should be: " << correctValue << endl;
-                isCorrect = false;
-            }
-        }
-    }
-
-    if (!isCorrect) {
-        std::cout << "WRONG TRANSFORMATION in " << implemName << "!" << endl;
-        return 1;
-    }
-
-    std::cout << "Correct transformation in " << implemName << "!" << endl;
-    return 0;
-}
-
 #endif
 
 void fill_input_matrix(Mat& matToFill, unsigned int pow, unsigned int seed = 10)
@@ -166,25 +96,6 @@ void fill_input_matrix(Mat& matToFill, unsigned int pow, unsigned int seed = 10)
             double real_part = (static_cast<double>(rand()) / RAND_MAX) * MAX_MAT_VALUES;
             double imag_part = (static_cast<double>(rand()) / RAND_MAX) * MAX_MAT_VALUES;
             matToFill(i, j) = std::complex<double>(real_part, imag_part);
-        }
-    }
-}
-
-template <class T>
-void fill_input_matrix(std::vector<std::vector<T>> &matToFill, unsigned int pow, unsigned int seed = 10)
-{
-    srand(time(nullptr)*seed*0.1);
-    unsigned int size = std::pow(2, pow); // Calculate the size of the matrix
-
-    
-    // Set input matrix as 2^pow x 2^pow matrix
-    matToFill.resize(size);
-
-    // Generate random complex numbers between 0.0 and and 250.0 and fill the matrix
-    for (unsigned int i = 0; i < size; ++i) {
-        matToFill[i].resize(size);
-        for (unsigned int j = 0; j < size; ++j) {
-            matToFill[i][j] = static_cast<T>((rand() % MAX_MAT_VALUES)  ); 
         }
     }
 }
@@ -231,24 +142,21 @@ int main(int argc, char *argv[]) {
     // set all it's needed to time the execution
     #if TIME_IMPL
     using clock = std::chrono::steady_clock;
-	using unitOfTime = std::chrono::duration<double, std::milli>;
-    const string unitTimeStr = "ms";
+	using unitOfTime = std::chrono::duration<double, std::micro>;
+    const string unitTimeStr = "\u03BCs";
 
     chrono::time_point<clock> begin;
     double total = 0.0;
     #endif
 
     // create the matrix xSpace to convert with FFT:
-    Mat xSpace(rowlength, rowlength); 
     const unsigned int pow = std::log2(ROW_LENGTH);
+    Mat xSpace, xSpaceToCheck;
+    Mat xFreq, correctXFreq;
     // Fill the matrix xSpace with random complex values:
     fill_input_matrix(xSpace, pow);
     
-    std::vector<std::vector<unsigned char>> vecXSpace;
-    std::vector<std::vector<std::complex<double>>> vecXFreq;
-    fill_input_matrix(vecXSpace, pow);
-
-    Mat xFreq(xSpace);
+    xFreq.resize(xSpace.rows(), xSpace.cols());
 
     #if CHECK_CORRECTNESS    
     // run the recursive version
@@ -313,42 +221,35 @@ int main(int argc, char *argv[]) {
         const string implementationName = "Sequential FFT 2D implementation";
         std::cout << "----------------"<< implementationName <<"----------------" << endl;
 
+        FFT_2D fft2D(xSpace, xFreq);
+
         unsigned int i = 0;
         #if TIME_IMPL
         total = 0.0;
         for( i = 0; i < iterToTime; i++ ){
-        #endif
-        
-        FFT_2D fft2D(xSpace, empty_matrix);
-
-        std::cout<<"Frequency values:" <<std::endl;
-        for (int j=0; j<xSpace.rows(); j++){
-            for(int k=0; k<xSpace.rows(); k++){
-                xSpace(j, k) = vecXSpace[j][k];
-            }
-        }
-        
-        #if TIME_IMPL
             begin = clock::now();
         #endif
-
-        fft2D.iterative_sequential(vecXSpace,vecXFreq);
+        
+        fft2D.transform_seq();
         
         #if TIME_IMPL
             double elapsed = chrono::duration_cast<unitOfTime>(clock::now() - begin).count();
             std::cout << "It took " << elapsed << unitTimeStr <<" in the execution number "<< i << endl;
             total += elapsed;
-            DFT_2D(xSpace,xSpace.rows());
-            #if CHECK_CORRECTNESS
-                checkCorrectness(implementationName, xSpace, vecXFreq);
-            #endif
-        #else
+        #endif
+
         #if CHECK_CORRECTNESS
-            checkCorrectness(implementationName, fft2D.getFrequencyValues(), xFreq);
+            DFT_2D(xSpace,correctXFreq);
+            checkCorrectness(implementationName, correctXFreq, xFreq);
+
+            std::cout << "Check correctness of the iFFT: " << std::endl;
+
+            fft2D.iTransform();
+            checkCorrectness("iFFT", fft2D.getSpatialValues(), xSpaceToCheck);
         #endif
-        #endif
+
         #if TIME_IMPL
-            fill_input_matrix(vecXSpace, pow, i+1);
+            fill_input_matrix(xSpace, pow, i+1);
         }
         std::cout << implementationName << " took on average: " << total/iterToTime << unitTimeStr << endl;
         #endif
@@ -356,22 +257,18 @@ int main(int argc, char *argv[]) {
     }
     #endif
 
-    Mat empty_mat(rowlength, rowlength);
     //parallel implementation: 
     #if PAR_IMPL
     {
         const string implementationName = "Parallel FFT 2D implementation";
         std::cout << "----------------"<< implementationName <<"----------------" << endl;
 
+        FFT_2D fft2D(xSpace, xFreq);
+
         unsigned int i = 0;
         #if TIME_IMPL
         total = 0.0;
         for( i = 0; i < iterToTime; i++ ){
-        #endif
-        
-        FFT_2D fft2D(xSpace, empty_mat);
-        
-        #if TIME_IMPL
             begin = clock::now();
         #endif
 
@@ -381,15 +278,17 @@ int main(int argc, char *argv[]) {
             double elapsed = chrono::duration_cast<unitOfTime>(clock::now() - begin).count();
             std::cout << "It took " << elapsed << unitTimeStr <<" in the execution number "<< i << endl;
             total += elapsed;
-            DFT_2D(xSpace,xSpace.rows());
-            #if CHECK_CORRECTNESS
-                checkCorrectness(implementationName, fft2D.getFrequencyValues(), xSpace);
-            #endif
-        #else
+        #endif
+
         #if CHECK_CORRECTNESS
-            checkCorrectness(implementationName, fft2D.getFrequencyValues(), xFreq);
+            DFT_2D(xSpace,correctXFreq);
+            checkCorrectness(implementationName, correctXFreq, xFreq);
+            
+            std::cout << "Check correctness of the inverse: " << std::endl;
+            fft2D.iTransform();
+            checkCorrectness("iFFT", fft2D.getSpatialValues(), xSpaceToCheck);
         #endif
-        #endif
+
         #if TIME_IMPL
             fill_input_matrix(xSpace, pow, i+1);
         }
