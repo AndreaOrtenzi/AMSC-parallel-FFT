a121b520d3cc73c5ef5ac8086f93b642c0c7022a
emanuelebroggini <emanuele.broggini@mail.polimi.it>
2023-09-04 20:16:04 +0200

    Create main to test MCU class

---
+++ IMG_compression/src/main.cpp
@@ -0,0 +1,23 @@
+#include "../inc/MinimumCodedUnit.hpp"
+#include "Eigen/Dense"
+
+
+int main(){
+
+    // const int numMatrices = 1; // Change this to the desired number of matrices
+    // Eigen::Matrix<double, MCU_SIZE, MCU_SIZE> matrixArray[numMatrices];
+
+    // // Initialize each matrix
+    // for (int i = 0; i < numMatrices; ++i) {
+    //     // You can populate each matrix 'matrixArray[i]' here as needed.
+
+    //     // For example, to change the value at (3,1) in the matrix at index 1 to 71:
+    //     if (i == 1) {
+    //         matrixArray[i](3, 1) = 71.0;
+    //     }
+    // }
+    MinimumCodedUnit img;
+    img.transform();
+    img.iTransform();
+    return 0;
+}
