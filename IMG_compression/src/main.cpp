84fbe9d9ffc0355bde0b68e34689d55a6613ced3
emanuelebroggini <emanuele.broggini@mail.polimi.it>
2023-09-07 02:04:31 +0200

    Update main.cpp

--- IMG_compression/src/main.cpp
+++ IMG_compression/src/main.cpp
@@ -4,42 +4,31 @@
 #include <string>
 
 int main(){
-    // TESTING MCU class with constructor commented: 
-    // MinimumCodedUnit img;
-    // img.transform();
-
-    // std::string path = "./imgs/compressed/test"; 
-    // // img.writeCompressedOnFile(path, 0);
-    // // img.readCompressedFromFile(path, 0);
-    // img.iTransform();
-
-    // img.printRestored();
-
-    // TESTING Image:
 
         // Specify folders paths and image name:
         std::string jpegImgsFolderPath = "./imgs/";
         std::string encodedFolderPath = "./imgs/compressed/";
         std::string imgName = "SpongebobJPG";
 
+        // Create a new istance of Image and read the input file image:
         Image image(jpegImgsFolderPath, encodedFolderPath, imgName, false);
 
-        // Read image and transform:
+        // Apply FFT:
         image.trasform();
 
-        // write compressed image:
+        // Write compressed image:
         image.writeCompressed();
 
-        // inverse transf:
+        // Apply Inverse transform:
         image.iTrasform();
 
         // Read compressed image:
         Image newImage(jpegImgsFolderPath, encodedFolderPath, imgName, true);
 
-        // Inverse transform:
+        // Apply iFFT:
         newImage.iTrasform();
 
-        // new encoded image:
+        // New encoded image:
         newImage.writeImage();
 
 
